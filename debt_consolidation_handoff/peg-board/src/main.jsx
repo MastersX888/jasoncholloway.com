1bb40bc67315cd2abb6eda411a790054521d8d2f
Jason Holloway <zh5779485@gmail.com>
2026-07-24 20:07:20 -0500

    checkpoint before checking out cursor/ops-dashboard-3e24

--- debt_consolidation_handoff/peg-board/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import PegBoard from "../../scp-peg-board.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <PegBoard />
-  </StrictMode>,
-);
